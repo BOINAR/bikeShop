cca443475713445c26e2848f0f2b39c41e5d77d6
BOINARIZIKI <nayeff.boinariziki@gmail.com>
2023-11-21 10:37:37 +0100

    final commit

---
+++ models/connection.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var options = {
+    connectTimeoutMS: 5000,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
+
+mongoose.connect('URI_BDD',
+    options,
+    function(err){
+        console.log(err);
+    }
+)
+
+module.exports = mongoose;
